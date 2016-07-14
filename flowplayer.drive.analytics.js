be62b2063cff5af50f23aaf836e9c89d0518442d
Niklas Närhinen <niklas@narhinen.net>
2016-07-14 09:53:23 +0300

    Attach progress handler on ready

--- flowplayer.drive.analytics.js
+++ flowplayer.drive.analytics.js
@@ -8,12 +8,12 @@
     xhr.onreadystatechange = function() {
       if (xhr.readyState === XMLHttpRequest.DONE && xhr.status !== 200) cb(new Error('Request error'));
     };
-    xhr.send(JSON.stringify({
+    xhr.send(JSON.stringify({ play: {
       src: src,
       seconds: time,
       p2p: p2p,
       playerVersion: playerVersion
-    }));
+    }}));
   }
 
   var extension = function(flowplayer) {
@@ -30,16 +30,16 @@
       player.on('ready', function(_ev, _api, video) {
         player.one('resume', function() {
           track(video.src, 0, flowplayer.version, false, _cbHandler);
+          player.on('progress', function(_ev, _api, time) {
+            lastProgress = time;
+            if (timeout) return;
+            timeout = setTimeout(function() {
+              track(player.video.src, lastProgress, flowplayer.version, null, _cbHandler);
+              clearTimeout(timeout);
+              timeout = null;
+            }, 5000);
+          });
         });
-      });
-      player.on('progress', function(_ev, _api, time) {
-        lastProgress = time;
-        if (timeout) return;
-        timeout = setTimeout(function() {
-          track(player.video.src, lastProgress, flowplayer.version, null, _cbHandler);
-          clearTimeout(timeout);
-          timeout = null;
-        }, 5000);
       });
     });
   };
