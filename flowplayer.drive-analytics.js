fca23a7e7c439a9d1f1ef46f21b8dee2767ac093
Niklas Närhinen <niklas@narhinen.net>
2016-09-29 21:51:50 +0300

    Integrate with xflow to support P2P stats

--- flowplayer.drive-analytics.js
+++ flowplayer.drive-analytics.js
@@ -9,27 +9,7 @@
 
 */
 (function() {
-  /* global flowplayer */
-
-  function track(src, time, playerVersion, p2p, finished, cb) {
-    if (typeof time !== 'number') return;
-    var trackerUrl = getTrackerURL(src);
-    if (!trackerUrl) return;
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', trackerUrl);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === XMLHttpRequest.DONE && xhr.status !== 200) cb(new Error('Request error'));
-    };
-    xhr.send(JSON.stringify({ play: {
-      src: src,
-      seconds: time,
-      p2p: p2p,
-      playerVersion: playerVersion,
-      finished: !!finished
-    }}));
-  }
+  /* global flowplayer, xflow */
 
   var extension = function(flowplayer) {
     flowplayer(function(player) {
@@ -42,18 +22,23 @@
       }
 
       player.on('ready', function(_ev, _api, video) {
+        var p2pTick = 0;
+        if (typeof xflow !== 'undefined') xflow.Broker.on(xflow.Events.STATS, function(stats) {
+          if (!stats.p2p) return;
+          p2pTick += stats.loaded;
+        });
         player.one(player.conf.splash ? 'progress' : 'resume', function() {
-          track(video.src, 0, flowplayer.version, false, false, _cbHandler);
+          track(video.src, 0, flowplayer.version, false, _cbHandler);
           nextProgess = 5;
           player.on('progress.drivetrack', function(_ev, _api, time) {
             if (time < nextProgess) return;
-            track(player.video.src, nextProgess, flowplayer.version, null, false, _cbHandler);
+            track(player.video.src, nextProgess, flowplayer.version, false, _cbHandler);
             nextProgess += 5;
           });
           player.on('finish.drivetrack', function() {
-            track(player.video.src, player.video.duration, flowplayer.version, null, true, _cbHandler);
+            track(player.video.src, player.video.duration, flowplayer.version, true, _cbHandler);
             player.one('resume', function() {
-              track(video.src, 0, flowplayer.version, false, false, _cbHandler);
+              track(video.src, 0, flowplayer.version, false, _cbHandler);
               nextProgess = 5;
             });
           });
@@ -61,6 +46,26 @@
             nextProgess = Math.ceil(time/5)*5;
           });
         });
+        function track(src, time, playerVersion, finished, cb) {
+          if (typeof time !== 'number') return;
+          var trackerUrl = getTrackerURL(src);
+          if (!trackerUrl) return;
+
+          var xhr = new XMLHttpRequest();
+          xhr.open('POST', trackerUrl);
+          xhr.setRequestHeader('Content-Type', 'application/json');
+          xhr.onreadystatechange = function() {
+            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status !== 200) cb(new Error('Request error'));
+          };
+          xhr.send(JSON.stringify({ play: {
+            src: src,
+            seconds: time,
+            p2pBytes: p2pTick,
+            playerVersion: playerVersion,
+            finished: !!finished
+          }}));
+          p2pTick = 0;
+        }
       });
     });
   };
